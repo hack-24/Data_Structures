8815ea527771c834e8cbc5565b5225d7e59a8f97
Navneet Ranjan <500067951@stu.upes.ac.in>
2020-07-14 12:17:08 +0530

    Creating and Displaying a linked list in C

--- LinkedListCreationandDisplay.c
+++ LinkedListCreationandDisplay.c
@@ -1,98 +1,67 @@
+//Display a Linked List
+
+
 #include <stdio.h>
 #include <stdlib.h>
-// declaring the Structure
-struct Node{
-    
-    int data;
-    struct Node*next;
+
+
+struct Node
+{
+int data;
+struct Node *next;
 }*first=NULL;
 
 
-
-
-
-int main(int argc, char **argv)
+void create(int A[],int n)
 {
-	int A[]={38,18,28,48,58,69};     //creating an araay
-     
-    create(A,6);       //Calling the Create Function
-    
-       display(first); //calling the display function
-    
-    RDisplay(first); //calling the recursive display function
-    
-    
-    printf(" The Linked List Contains %d nodes \n ",Count(first));
-    printf("The Sum is %d \n " ,RSum(first));
-    printf("Max is %d \n",RMax(first));
-    
-    return(0);
+int i;
+struct Node *t,*last;                                //declaraing a pointer t and last of type struct Node
+first=(struct Node *)malloc(sizeof(struct Node));    //creating a node in heap using malloc
+first->data=A[0];                                    // assigning the first value in link list to data[0]
+first->next=NULL;
+last=first;                                           //Assigning value of first node to last
+for(i=1;i<n;i++)
+{
+t=(struct Node*)malloc(sizeof(struct Node));           
+t->data=A[i];                                         //Assigning the value of A[i] to t->data
+                            
+t->next=NULL;                                          // Assigning t->next =NULL
+last->next=t;                                          // Assigning last->next =t 
+last=t;                                                //Assigning last=t
+}
+}
+
+
+void Display(struct Node *p)                           //Display Function
+{ 
+while(p!=NULL)                                        //Checking if  p is NULL or not
+{
+printf("%d ",p->data);                                //printing the value inside node
+p=p->next;                                            //assigning the value of p->next to p
+}
+
 }
 
 
 
-void create(int A[],int n){
-    
-    
-    
-    
-    int i;
-    struct Node *t,*last;     //Declaring  a pointer t and last of type struct Node *
-    
-    
-    first=(struct Node*)malloc(sizeof(struct Node)); //Creating a node of size (Struct node)
-    
-    first->data=A[0];  //assigning the first element of the array to first->data
-	
-    first->next=NULL;  // assigning NULL to first->next
-    
-    
-    last=first;      //assigning the first node the value of last node
-    
-    
-    for(i=1;i<n;i++){
-        
-        
-        t=(struct Node *)malloc(sizeof(struct Node));      //create n nodes of size struct Node
-        t->data=A[i];  // Assigning A[i] to t->data
-        t->next=NULL;
-        
-        last->next=t;
-        last =t;
-        
-    }
-    
-    
+
+void RDisplay(struct Node *p)                          //Recursive Display function
+{
+if(p!=NULL)
+{
+RDisplay(p->next);                                     //Recursive Call to RDisplay
+printf("%d ",p->data);                                 //printing the data
 }
-
-
-void display(struct Node *p){
-    
-    
-    
-    while(p!=NULL){
-        
-        
-        printf("%d ",p->data); //printing the elements of linked list
-        p=p->next;
-    }
-    
-    
 }
 
 
 
-void RDisplay(struct Node *p){
-    
-    if(p!=NULL){
-        
-        
-      
-        RDisplay(p->next); //recursive call to RDisplay
-          printf("%d ",p->data);
-    }
-    
-    printf(" \n ");
-    
-    
+int main()
+{
+struct Node *temp;
+int A[]={3,5,7,10,25,8,32,2};                           //Array decalaration and initialization
+
+create(A,8);                                             // calling create function
+Display(first);                                          //Calling Display Function
+return 0;
 }
